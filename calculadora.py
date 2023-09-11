0ba537b0c018f2acb1dacea11e553acf712322d1
axelr <axrlr4178@gmail.com>
2023-09-11 12:45:52 -0500

    Programa de Calculadora

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+'''
+autor: Axel Almaguer
+Fecha: 11 Septiembre
+Descripcion: Calculadora que sume
+'''
+
+a = input("Ingresa el primer valor ")
+b = input("Ingresa el segundo valor ")
+print(type(a), type(b))
+print("suma ", a + b)
+a = float (a)
+b = float (b)
+print(type(a), type(b))
+print("suma ", a + b)
+
+print(type(a), type(b))
+print("resta ", a - b)
+
+print(type(a), type(b))
+print("division ", a / b)
+
+print(type(a), type(b))
+print("multiplicacion ", a * b)
