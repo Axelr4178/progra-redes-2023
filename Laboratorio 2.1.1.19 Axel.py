95118958cafee996d0ef0f6680132ad987132fd6
Axelr4178 <61528281+Axelr4178@users.noreply.github.com>
2023-09-18 11:19:48 -0600

    Add files via upload

---
+++ Laboratorio 2.1.1.19 Axel.py
@@ -0,0 +1,26 @@
+﻿'''
+    Autor: Axel Alejandro Almaguer Rodriguez
+    Laboratorio 2.1.1.19
+'''
+
+print("       *         "* 2)
+print("      * *        "* 2)
+print("     *   *       "* 2)
+print("    *     *      "* 2)
+print("   *       *     "* 2)
+print("  *         *    "* 2)
+print(" *           *   "* 2)
+print("*****     *****  "* 2)
+print("    *     *      "* 2)
+print("    *     *      "* 2)
+print("    *     *      "* 2)
+print("    *     *      "* 2)
+print("    *     *      "* 2)
+print("    *     *      "* 2)
+print("    *     *      "* 2)
+print("    *******      "* 2)
+
+
+print("    *\n   * *\n  *   *\n *     *")
+print("***   ***\n  *   *\n  *   *")
+print("  *****")
