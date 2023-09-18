dba586d321d4228297730b9073c813c5d2314f4c
Axelr4178 <61528281+Axelr4178@users.noreply.github.com>
2023-09-18 11:11:20 -0600

    Add files via upload

---
+++ Laboratorio 2.1.1.18.py
@@ -0,0 +1,7 @@
+﻿'''
+    Autor: Axel Alejandro Almaguer Rodriguez
+    Laboratorio 2.1.1.18
+'''
+
+print("Fundamentos","Programación","en",sep="***",end="...")
+print("Python")
---
+++ laboratorio 2.1.1.19.py
@@ -0,0 +1,24 @@
+﻿'''
+    Autor: Axel Alejandro Almaguer Rodriguez
+    Laboratorio 2.1.1.19
+'''
+
+print("       *         "* 2)
+print("      * *        "* 2)
+print("     *   *       "* 2)
+print("    *     *      "* 2)
+print("   *       *     "* 2)
+print("  *         *    "* 2)
+print(" *           *   "* 2)
+print("*****     *****  "* 2)
+print("    *     *      "* 2)
+print("    *     *      "* 2)
+print("    *     *      "* 2)
+print("    *     *      "* 2)
+print("    *     *      "* 2)
+print("    *     *      "* 2)
+print("    *     *      "* 2)
+print("    *******      "* 2)
+
+
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
