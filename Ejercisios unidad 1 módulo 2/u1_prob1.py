9e45f0b5d910f5f012267d1c6c43bd78afbd10b6
Axelr4178 <61528281+Axelr4178@users.noreply.github.com>
2023-10-02 11:46:07 -0600

    Add files via upload

---
+++ Ejercisios unidad 1 módulo 2/u1_prob1.py
@@ -0,0 +1,28 @@
+# Autor: Axel Alejandro Almaguer Rodriguez 
+#El Tremendo Sat
+#Fecha de cración:2 de octubre del 2023
+
+menos10 = 10000
+menos20 = 20000
+menos35 = 35000
+menos60 = 60000
+masde60 = 60001
+
+print ("Ingresa la cantidad a ingresar")
+cant = input()
+for (cant > menos10)
+impuesto5 = 5*cant/100
+print (impuesto5)
+if (cant > menos20),
+    impuesto15 = 15*cant/100
+print (impuesto15)
+if (cant > menos35),
+    impuesto20 = 20*cant/100
+print (impuesto20),
+if (cant > menos60),
+    impuesto30 = 30*cant/100
+print (impuesto30)
+if (cant < masde60),
+    impuesto45 = 45*cant/100
+print (impuesto45)
+    
---
+++ Ejercisios unidad 1 módulo 2/u1_prob2.py
@@ -0,0 +1,30 @@
+# Autor: Axel Alejandro Almaguer Rodriguez 
+#Jugando con listas
+#Fecha de cración:2 de octubre del 2023
+
+M1 = ("probabilidad y Estadística")
+M2 = ("Electrónica para IDC Física")
+M3 = ("Conexión de redes WAN")
+M4 = ("Administración de servidores I")
+M5 = ("Programación de Redes")
+M6 = ("Inglés IV")
+
+print ( "¿Cuál es la calificación que obteniste en:",M1"?")
+C1 = input()
+print ("¿Cuál es la calificación que obteniste en:",M2"?")
+C2 = input()
+print ("¿Cuál es la calificación que obteniste en:",M3"?")
+C3 = input()
+print ("¿Cuál es la calificación que obteniste en:",M4"?")
+C4 = input()
+print ("¿Cuál es la calificación que obteniste en:",M5"?")
+C5 = input()
+print ("¿Cuál es la calificación que obteniste en:",M6"?")
+C6 = input()
+
+print ("En",M1 "has sacado", C1)
+print ("En",M2 "has sacado", C2)
+print ("En",M3 "has sacado", C3)
+print ("En",M4 "has sacado", C4)
+print ("En",M5 "has sacado", C5)
+print ("En",M6 "has sacado", C6)
